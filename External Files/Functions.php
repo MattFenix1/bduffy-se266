063a6d72316a16fc440a0fbcf58ec392f8b111c5
MattFenix1 <bjduffy@email.neit.edu>
2024-04-11 13:30:52 -0400

    End of W3

--- External Files/Functions.php
+++ External Files/Functions.php
@@ -13,3 +13,31 @@ function calcResults(){
     $fname = filter_input(INPUT_POST, 'fname');
     $lname = filter_input(INPUT_POST, 'lname');
 }
+
+function age($bdate){
+    $date = new DateTime($bdate);
+    $now = new DateTime();
+    $interval = $now->diff($date);
+    return $interval->y;
+}
+
+function bmi($ft, $in, $weight){
+    $kg = $weight/2.20462;
+    $height = ($ft*12+$in) * 0.0254;
+    $bmi = $weight/($height*$height);
+}
+
+function bmiDescription($bmi){
+    if($bmi < 18.5){
+        echo 'You are UnderWeight.';
+    }
+    else if($bmi>= 18.5 and $bmi <= 24.9){
+        echo 'You are Normal Weight.';
+    }
+    else if($bmi >= 25.0 and $bmi <= 29.9){
+        echo 'You are OverWeight.';
+    }
+    else{
+        echo 'You are Obese.';
+    }
+}
--- week2/Patient_Intake_Form.php
+++ week2/Patient_Intake_Form.php
@@ -15,6 +15,7 @@
         $heightinch = '';
         $weight = '';
 
+        require '../External Files/Functions.php';
         if (isset($_POST['submit'])){ //Variables after submit button is pressed
             $fname = filter_input(INPUT_POST, 'fname');
             $lname = filter_input(INPUT_POST, 'lname');
@@ -23,6 +24,8 @@
             $heightft = filter_input(INPUT_POST, 'heightft', FILTER_VALIDATE_FLOAT);
             $heightinch = filter_input(INPUT_POST, 'heightinch', FILTER_VALIDATE_FLOAT);
             $weight = filter_input(INPUT_POST, 'weight', FILTER_VALIDATE_FLOAT);
+            $age = age($birthdate);
+            $bmi = bmi($heightft, $heightinch, $weight);
         }
     ?>
     <form method="POST"> <!--Beginning of form-->
@@ -32,13 +35,13 @@
         <label for="lname">Last Name:</label><br> <!--Last Name for form-->
         <input type="text" id="lname" name="lname" value="<?= $_POST['lname'] ?>" required>
         
-        <p>Are you married?</p> <!--Choosing whether or not they are married-->
+        <p>Are you married?</p> <!--Choosing whether or not they are married, would not show up in results-->
         <input type="radio" id="yes" name="married" <?php if (isset($married) && $married == "yes") echo "checked"; ?> value="yes">
         <label for="yes">Yes</label>
         <input type="radio" id="no" name="married" <?php if (isset($married) && $married == "no") echo "checked"; ?> value="no">
         <label for="no">No</label><br>
 
-        <label for="birthdate">Birth Date:</label> <!--Entering their birthdate-->
+        <label for="birthdate">Birth Date:</label> <!--Entering their birthdate, would not show up in results-->
         <input type="date" id="birthdate" name="birthdate" max="2024-04-02" value="<?= $_POST['birthdate'] ?>" required><br>
 
         <label for="height">Height:</label><br> <!--Entering their height in feet then inches-->
@@ -61,6 +64,9 @@
         <p><span>Birthdate:</span> <?= $birthdate; ?></p>
         <p><span>Height:</span> <?= $heightft; ?> Feet <?= $heightinch; ?> Inches</p>
         <p><span>Weight:</span> <?= $weight; ?></p>
+        <p><span>Age:</span> <?= age($birthdate)?></p> <!--Could not get to show-->
+        <p><span>BMI:</span> <?= $bmi?></p> <!--Could not get to show-->
+        <p><span>BMI Classification:</span> <?= bmiDescription($bmi)?></p>
     <?php endif; ?>
 </body>
 </html>
